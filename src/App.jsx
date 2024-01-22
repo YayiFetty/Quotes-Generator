291dc1664a1e6d1b133b3668e7a05be5d28542e6
YayiFetty <yayinaz00>
2024-01-22 14:10:04 +0100

    comnit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const BASE_URL = "https://type.fit/api/quotes";
-
-export default function App() {
-  const [quote, setQuote] = useState("");
-
-  async function fetchQuote() {
-    try {
-      const response = await fetch(BASE_URL);
-      const data = await response.json();
-      const randomIndex = Math.floor(Math.random() * data.length);
-      setQuote(data[randomIndex].text);
-    } catch (error) {
-      console.error("Error fetching quote:", error);
+function App() {
+  const initial = {text:"", author:""}
+  const [quotes, setQuotes] = useState(initial);
+  
+  async function fetchQuote(){
+    try{
+        const res = await fetch(`${BASE_URL}`);
+        const data = await res.json();
+        const randomLength = Math.floor((Math.random()) * data.length)
+        const randomIndex = data[randomLength]
+        setQuotes({text:randomIndex.text, author:randomIndex.author});
+    }
+    catch(error){
+        console.error("Error fetched", error)
     }
   }
-
+  useEffect(function(){
+    fetchQuote();
+  },[])
   return (
-    <div>
-      <button onClick={fetchQuote}>Generate</button>
-      <p>{quote}</p>
+    <div className='max-w-md bg-slate-500 flex flex-col items-center justify-center mx-auto w-full h-60 '>
+       <button onClick={fetchQuote} className='w-fit h-fit p-2 bg-black text-white'>Generate</button>
+   
+          <div className='flex flex-col'>
+            <p>  {quotes.text}</p>
+            <p>{quotes.author}</p>   
+         </div>
     </div>
-  );
+  )
 }
+
+export default App
