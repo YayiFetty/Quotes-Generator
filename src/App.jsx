7b7d89f1614060ff4b0c9a23fcd94d12c654be77
YayiFetty <yayinaz00>
2024-01-22 12:48:54 +0100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+
+const BASE_URL = "https://type.fit/api/quotes";
+
 export default function App() {
+  const [quote, setQuote] = useState("");
+
+  async function fetchQuote() {
+    try {
+      const response = await fetch(BASE_URL);
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      setQuote(data[randomIndex].text);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  }
+
   return (
-      <div>
-      
-      </div>
-  )
+    <div>
+      <button onClick={fetchQuote}>Generate</button>
+      <p>{quote}</p>
+    </div>
+  );
 }
