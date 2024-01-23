23aa81618a1fe5bfb1361da17863de12ca2fc699
YayiFetty <yayinaz00>
2024-01-23 01:11:44 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Quotes-Generator/',
+  build: {
+    outDir: 'dist', // Specify the output directory for the build artifacts
+  },
 })
